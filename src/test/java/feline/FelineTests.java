77a1de8ad1ee44d6703a4cd6fa1b92d2b4adc0a1
Alevtina Buldakova <alkafilatova0@gmail.com>
2024-05-16 12:50:47 +0300

    group tests and formated,rename metods

--- src/test/java/feline/FelineTests.java
+++ src/test/java/feline/FelineTests.java
@@ -1,4 +1,5 @@
 package feline;
+
 import com.example.Feline;
 import net.datafaker.Faker;
 import org.junit.Assert;
@@ -7,32 +8,38 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
+
 @RunWith(MockitoJUnitRunner.class)
-public class FelineEatMeatTest {
+public class FelineTests {
     @Spy
     Feline feline = new Feline();
+
     @Test
     public void felineEatMeat() throws Exception {
-        List<String> expectedResult = List.of("Животные","Птицы","Рыба");
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
         Mockito.when(feline.getFood("Хищник")).thenReturn(expectedResult);
         List<String> actualResult = feline.eatMeat();
-        Assert.assertEquals("Ожидаемый результат не соответствует фактическому: ",expectedResult,actualResult);
+        Assert.assertEquals("Ожидаемый результат не соответствует фактическому: ", expectedResult, actualResult);
     }
+
     @Test
-    public void felineGetKittens(){
+    public void felineGetKittens() {
         feline.getKittens();
-        Mockito.verify(feline,Mockito.times(1)).getKittens(1);
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
     }
+
     @Test
-    public void felineGetKittensWithParameters(){
+    public void felineGetKittensWithParameters() {
         Faker faker = new Faker();
-        int randomkittensCount = faker.number().numberBetween(1,10);
-        Assert.assertEquals(randomkittensCount,feline.getKittens(randomkittensCount));
+        int randomkittensCount = faker.number().numberBetween(1, 10);
+        Assert.assertEquals(randomkittensCount, feline.getKittens(randomkittensCount));
     }
+
     @Test
-    public void felineGetFamily(){
+    public void felineGetFamily() {
         Feline felineNew = new Feline();
-        Assert.assertEquals("Кошачьи",felineNew.getFamily());
+        Assert.assertEquals("Кошачьи", felineNew.getFamily());
     }
 }
