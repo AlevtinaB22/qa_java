c61568c70b2d6e4ec4bc619b879939d4704f2971
Alevtina Buldakova <alkafilatova0@gmail.com>
2024-05-16 12:52:32 +0300

    group tests and formated,rename metods,change spy,add exceptiontest

--- src/test/java/lion/LionTests.java
+++ src/test/java/lion/LionTests.java
@@ -1,27 +1,36 @@
 package lion;
+
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
-public class LionGetFoodTest {
+
+public class LionTests {
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
     }
+
     @Test
     public void lionGetFood() throws Exception {
         Feline feline = Mockito.mock(Feline.class);
-        Lion lion = new Lion("Самец",feline);
-        List<String> expectedResult = List.of("Животные","Птицы","Рыба");
+        Lion lion = new Lion("Самец", feline);
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
         Mockito.when(feline.getFood("Хищник")).thenReturn(expectedResult);
         List<String> actualResult = lion.getFood();
-        Assert.assertEquals("Ожидаемый результат не соответствует фактическому: ",expectedResult,actualResult);
+        Assert.assertEquals("Ожидаемый результат не соответствует фактическому: ", expectedResult, actualResult);
+    }
+
+    @Test
+    public void lionGetKittens() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(2);
+        Assert.assertEquals("Ожидаемый результат не соответствует фактическому:", 2, lion.getKittens());
     }
 }
